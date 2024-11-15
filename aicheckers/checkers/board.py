61cbcdc43590311f06926820e49584a32b7e00b7
Phoenix Games <118949998+Cybrock9000@users.noreply.github.com>
2024-11-15 12:37:31 -0600

    Update board.py

--- aicheckers/checkers/board.py
+++ aicheckers/checkers/board.py
@@ -14,4 +14,3 @@ def draw_squares(self, win):
       for col in range(row % 2, ROWS, 2):
         pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
         
-def
