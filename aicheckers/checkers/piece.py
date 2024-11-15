36dfc5821cb3345691e9ed02ac04d4e7039bb62d
Phoenix Games <118949998+Cybrock9000@users.noreply.github.com>
2024-11-15 12:43:19 -0600

    Create piece.py

---
+++ aicheckers/checkers/piece.py
@@ -0,0 +1,10 @@
+from .constance import *
+class Piece:
+  def __intit__(self, row, col, color):
+    self.row = row
+    self.col = col
+    self.color = color
+    self.king = False
+
+    if self.color == 
+    self.direction = 1
