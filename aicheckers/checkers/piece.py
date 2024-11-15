5d692a45b420866a7d97fe5781feb38597827fc7
Phoenix Games <118949998+Cybrock9000@users.noreply.github.com>
2024-11-15 12:53:55 -0600

    Update piece.py

--- aicheckers/checkers/piece.py
+++ aicheckers/checkers/piece.py
@@ -1,10 +1,31 @@
 from .constance import *
 class Piece:
+  PADDING = 10
+  OUTLINE = 2
   def __intit__(self, row, col, color):
     self.row = row
     self.col = col
     self.color = color
     self.king = False
 
-    if self.color == 
-    self.direction = 1
+    if self.color == RED:
+      self.direction = -1
+    else:
+      self.direction = 1
+  self.x = 0
+  self.y = 0
+
+def cal_pos(self):
+  self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+  self/.y =  SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+def make_King(self):
+  self.king = True
+
+def draw(self, win):
+  radius = SQUARE_SIZE//2 - self.PADDING
+  pygame.draw.circle(win, self.color, (self.x, self.y), radius +self.OUTLINE)
+  pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+
+
+
